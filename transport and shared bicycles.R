6e35904e452a7b68362ee8cd5902a8f282c9edda
limoda <limoda@126.com>
2017-05-27 14:00:40 +0800

    Add files via upload

---
+++ transport and shared bicycles.R
@@ -0,0 +1,42 @@
+#数据导入#
+read.csv("traf.csv")
+traf <- read.csv("traf.csv")
+traffic <- traf[,3:12]
+
+#OLS回归分析#
+lm1 <- lm(bs~pa+pc+busdensity+bus+taxi+rushhour+speed+jam+time+stopcover,data = traf)
+print(lm1)
+summary(lm1)
+
+#相关分析#
+library(corrplot)
+options(digits = 2)
+cor1 <- cor(traffic)
+print(cor1)
+corrplot(cor1,"shade")
+
+#多重共线性检验#
+library(car)
+vif1 <- vif(lm1)
+print(vif1)
+
+#岭回归#
+library(MASS)
+plot(lm.ridge(bs~pa+pc+busdensity+bus+taxi+rushhour+speed+jam+time+stopcover,data = traf,lambda=seq(0,0.9,0.001)))
+select(lm.ridge(bs~pa+pc+busdensity+bus+taxi+rushhour+speed+jam+time+stopcover,data = traf,lambda=seq(0,0.9,0.001)))
+print(rlm)
+
+#因子分析#
+fa <- factanal(traffic,factors = 3,rotation = "none")
+print(fa)
+fa2 <- factanal(traffic,factors = 3)
+print(fa2)
+fa3 <- factanal(traffic,factors=3,scores = "regression")
+print(fa3$scores)
+library(mvstats)
+rank1 <- factanal.rank(fa3,plot = T)
+print(rank1$Ri)
+rank2 <- rank1$Ri[,1]
+plot(rank2,traf$bs)
+cor(rank2,traf$bs)
+
